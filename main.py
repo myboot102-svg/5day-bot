dc5fa6f2654d2dc656dd37e727c658dfb75043b1
myboot102-svg <myboot102@gmail.com>
2026-08-15 05:05:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("5day Bot is ready!")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "أهلاً بيج في بوت 5day.\n\nاختاري من القائمة للبدء."
+    )
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
